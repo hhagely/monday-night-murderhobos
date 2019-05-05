204b3cea4b868836cf0daab045411dd7fcdb7e42
Herb Hagely <herb.hagely@gmail.com>
2019-05-04 21:20:54 -0500

    Making party-treasury table more responsive.

--- web/src/components/party-treasury/party-treasury.js
+++ web/src/components/party-treasury/party-treasury.js
@@ -4,51 +4,52 @@ import React from 'react';
 import styles from './party-treasury.module.css';
 
 function PartyTreasury({ items }) {
-  // const { _rawBody, authors, campaign, title, mainImage, publishedAt } = props;
   return (
-    <table className={styles.treasuryTable}>
-      <thead>
-        <tr>
-          <th>Item Name</th>
-          <th>Value</th>
-          <th>Belongs To</th>
-          <th>Player</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map(item => {
-          const { id, owner } = item;
-          const { itemName, link, value } = item.item;
-          const { character, race, person } = owner;
+    <div className="tableContainer">
+      <table className={styles.treasuryTable}>
+        <thead>
+          <tr>
+            <th>Item Name</th>
+            <th>Value</th>
+            <th>Belongs To</th>
+            <th>Player</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item) => {
+            const { id, owner } = item;
+            const { itemName, link, value } = item.item;
+            const { character, race, person } = owner;
 
-          return (
-            <tr key={id}>
-              <th>
-                <div className={styles.charInfo}>
-                  <a href={link} target="_blank" rel="noopener noreferrer">
-                    {itemName}
-                  </a>
-                </div>
-              </th>
-              <th>
-                <div className={styles.charInfo}>{value}</div>
-              </th>
-              <th>
-                <div className={styles.charInfo}>{`${
-                  character.characterName
-                }`}</div>
-                <div className={styles.charSubInfo}>{`${race} ${
-                  owner.class
-                }`}</div>
-              </th>
-              <th>
-                <div className={styles.charInfo}>{person.name}</div>
-              </th>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+            return (
+              <tr key={id}>
+                <th>
+                  <div className={styles.charInfo}>
+                    <a href={link} target="_blank" rel="noopener noreferrer">
+                      {itemName}
+                    </a>
+                  </div>
+                </th>
+                <th>
+                  <div className={styles.charInfo}>{value}</div>
+                </th>
+                <th>
+                  <div className={styles.charInfo}>{`${
+                    character.characterName
+                  }`}</div>
+                  <div className={styles.charSubInfo}>{`${race} ${
+                    owner.class
+                  }`}</div>
+                </th>
+                <th>
+                  <div className={styles.charInfo}>{person.name}</div>
+                </th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
